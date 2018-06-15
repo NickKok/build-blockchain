ca5e7c6b93b86cab9e2840ba4dc5af9c123ad967
NickKok <nikoskokkini@gmail.com>
2018-06-15 11:05:04 +0200

    Create blockchain API to Post a new block and GET the blocks of chain

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 //App Class
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -8,9 +9,21 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
+//Run npm run dev
+//Check out the this link on your browser or on postman : localhost:3001/blocks
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
 });
 
+//Run npm run dev
+//Use  postman  to make a post request on: localhost:3001/mine having on the body json data
+app.post('/mine', (req, res)=> {
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+
+  res.redirect('/blocks');
+});
 
 app.listen(HTTP_PORT, () => console.log(`Listening on the port ${HTTP_PORT}`));
