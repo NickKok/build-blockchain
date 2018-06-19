f7630f54d6d32c6557757437b81772d354e71cd5
NickKok <nikoskokkini@gmail.com>
2018-06-19 10:51:40 +0200

    Implement Transaction verification function and some tests

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -33,8 +33,11 @@ class Transaction {
       amount : senderWallet.balance,
       address : senderWallet.publicKey,
       signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
-
     }
+  }
+
+  static verifyTransaction(transaction){
+    return ChainUtil.verifySignature(transaction.input.address, transaction.input.signature, ChainUtil.hash(transaction.outputs));
 
   }
 
